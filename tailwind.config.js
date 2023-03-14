f671287aab6449dc60fa38f23cd8540138cd136d
Alexander <alexandergaruba96@gmail.com>
2023-03-14 09:27:48 +0100

    tailwind css and livewire installation

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/**/*.blade.php",
+    "./resources/**/**/**/*.blade.php",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+};
